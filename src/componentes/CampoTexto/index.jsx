840359afef21bc0d2835249b7c8e10a0497e0654
brenoiscde <breninhoponticelli@gmail.com>
2025-01-30 20:54:25 -0300

    recebendo o valor do input com onChange

--- src/componentes/CampoTexto/index.jsx
+++ src/componentes/CampoTexto/index.jsx
@@ -2,10 +2,15 @@ import "./CampoTexto.css";
 
 function CampoTexto(props){
     let p = props;
+    let valor = "";
+    const aoDigitar = (event) =>{
+        valor = event.target.value;
+        console.log(valor);
+    };
     return(
         <div className="campo-texto" >
             <label>{p.label}</label>
-            <input required={p.obrigatorio} placeholder={p.placeholder}/>
+            <input value={valor} onChange={aoDigitar} required={p.obrigatorio} placeholder={p.placeholder}/>
         </div>
     );
 }
